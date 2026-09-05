5997afba1b95364ead348aae90bdf5dafbb4e844
espisepi <sepinaco@gmail.com>
2026-09-06 01:04:25 +0200

    ng generate angular-three-plugin:init (without scenegraph option)

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,10 +2,8 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideNgtRenderer } from 'angular-three/dom';
 
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
-  ]
+  providers: [provideBrowserGlobalErrorListeners(), provideRouter(routes), provideNgtRenderer()],
 };
